468b7629ee0dc4b69d33e128a98b7fb4b4f80dc3
johnmrox <johnmrox@gmail.com>
2017-11-08 15:17:09 -0500

    ngif section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+	selector: 'app-root',
+	styleUrls: ['app.component.scss'],
+	template: `
+		<div class="app">
+			<input 
+				type="text"
+				[value]="name"
+				(input)="handleChange($event.target.value)">
+			<div *ngIf="name.length > 2">
+				Searching for...{{ name }}
+			</div>
+		</div>
+	`
 })
 export class AppComponent {
-
+	name: string = '';
+	handleChange(value: string) {
+		this.name = value;
+	}
 }
