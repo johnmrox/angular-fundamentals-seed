87f8f7a6b3e80e1fbc4fb12ff9f684802dfd5882
johnmrox <johnmrox@gmail.com>
2017-11-08 18:58:51 -0500

    creating a container component section

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
 	declarations: [AppComponent],
 	bootstrap: [AppComponent],
-	imports: [BrowserModule, CommonModule]
+	imports: [
+		//angular modules
+		BrowserModule,
+		CommonModule,
+		//custom modules
+		PassengerDashboardModule
+	]
 })
 export class AppModule {}
