a17d9eae7fa52bbac1bfd95ad5fcb574f74f551e
johnmrox <johnmrox@gmail.com>
2017-11-08 19:34:14 -0500

    presentational (dumb) components section

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
 
+//components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+
 @NgModule({
-	declarations: [PassengerDashboardComponent],
+	declarations: [PassengerDashboardComponent, PassengerCountComponent, PassengerDetailComponent],
 	imports: [CommonModule],
-	exports: [PassengerDashboardComponent]
+	exports: [PassengerDashboardComponent] //PassengerCountComponent and PassentgerDetailComponent don't need to be exported since app.component.ts only uses the passenger dashboard component
 })
 export class PassengerDashboardModule {}
